330ffd985300a4f1f6527bf70fd735928f0e0fcc
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2018-09-07 17:11:10 -0700

    Added offline dictionary and thesaurus json files
    - Converted the data to a dictionary and saved to userDefaults as dictionaries

---
+++ Supporting/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  VocabBuilder
+//
+//  Created by Memo on 9/7/18.
+//  Copyright © 2018 Memo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct UD {
+    
+    static let defaults = UserDefaults.standard
+    
+    static var dictionary = defaults.dictionary(forKey: "dictionary")
+    static var thesaurus = defaults.dictionary(forKey: "thesaurus")
+    
+}
+
